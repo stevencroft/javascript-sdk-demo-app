87257ca0eae710d28a1dccb69bcc4262d1b53e2e
Steve C <34257347+stevencroft@users.noreply.github.com>
2022-05-24 16:15:17 +1000

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 // constants.js
 
 // Default datafile provided to get up and running quickly. Replace with your own!
-const datafileURL = 'https://cdn.optimizely.com/datafiles/CpqC7ch493FEqa3HzY3963.json';
+const datafileURL = 'https://cdn.optimizely.com/datafiles/kFnXtUjfAZvmarH7w1s8L.json';
 
 module.exports = {
   datafileURL
